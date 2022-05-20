85e9af56c7979c1accaed4faa2e303b5d8bef845
Dima Ukraintsev <d.ukrain.yandex.ru>
2022-05-20 17:07:46 +0300

    name input logic

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
+/*jshint esversion: 6 */
+
+import userName from '../src/cli.js';
+
+userName();
 console.log('Welcome to the Brain Games!');
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+import readlineSync from 'readline-sync';
+
+function userName() {
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + name + '!');
+}
+
+export default userName;
