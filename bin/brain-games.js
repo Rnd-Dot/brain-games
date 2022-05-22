19be71dd0a294058c2ab34d18e9a0e94b408f0a3
Dima Ukraintsev <d.ukrain.yandex.ru>
2022-05-22 13:57:17 +0300

    corrections

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-/* jshint esversion: 6 */
 
 import userName from '../src/cli.js';
 
 userName();
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!'); 
