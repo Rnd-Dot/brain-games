86f11c61da8b86b0623c6591d9cab1d8e6cf9f1b
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-05-26 12:03:34 +0300

    +brain-gcd

---
+++ src/game/gcd.js
@@ -0,0 +1,25 @@
+import getRandomNumber from '../conditions.js';
+import runGame from '../play.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (a, b) => {
+    if (a == 0) return b;
+
+    while (b != 0) {
+        if (a > b) a -= b;
+        else b -= a;
+    }
+
+    return a;
+};
+
+const getGameData = () => {
+    const num1 = getRandomNumber(0, 100);
+    const num2 = getRandomNumber(0, 100);
+    const answer = String(gcd(num1, num2));
+    const question = `${num1} ${num2}`;
+    return [question, answer];
+};
+
+export default () => runGame(description, getGameData);
