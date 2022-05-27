b86b26b9d4a0ef2286e6bf3b4fbf8e7f901f113c
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-05-27 11:05:18 +0300

    +brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainPrime from '../src/game/prime.js';
+
+brainPrime();
---
+++ src/game/prime.js
@@ -0,0 +1,22 @@
+import getRandomNumber from '../random.js';
+import runGame from '../main.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+    for (let i = 2; i < num; i += 1) {
+        if (num % 2 === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+};
+
+const getGameData = () => {
+    const number = getRandomNumber(0, 50);
+    const question = `${number}`;
+    const answer = isPrime(number) ? 'yes' : 'no';
+    return [question, String(answer)];
+};
+
+export default () => runGame(description, getGameData);
