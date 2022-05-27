009868c343cb7d85dcee332d5fbb9fa13125ea8d
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-05-27 14:19:18 +0300

    fix prime

--- src/game/prime.js
+++ src/game/prime.js
@@ -5,7 +5,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (num) => {
     for (let i = 2; i < num; i += 1) {
-        if (num % 2 === 0) {
+        if (num % i === 0) {
             return false;
         }
     }
