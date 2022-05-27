01a49a537fecc16b75595d42065a732b6cad7401
Dima Ukraintsev <d.ukrain@yandex.ru>
2022-05-27 14:37:00 +0300

    fix lint

--- src/game/gcd.js
+++ src/game/gcd.js
@@ -4,21 +4,21 @@ import runGame from '../main.js';
 const description = 'Find the greatest common divisor of given numbers.';
 
 const gcd = (a, b) => {
-    if (a === 0) {
-        return b;
-    }
-    while (b !== 0) {
-        if (a > b) { a -= b; } else { b -= a; }
-    }
-    return a;
+  if (a === 0) {
+    return b;
+  }
+  while (b !== 0) {
+    if (a > b) { a -= b; } else { b -= a; }
+  }
+  return a;
 };
 
 const getGameData = () => {
-    const num1 = getRandomNumber(0, 100);
-    const num2 = getRandomNumber(0, 100);
-    const answer = String(gcd(num1, num2));
-    const question = `${num1} ${num2}`;
-    return [question, answer];
+  const num1 = getRandomNumber(0, 100);
+  const num2 = getRandomNumber(0, 100);
+  const answer = String(gcd(num1, num2));
+  const question = `${num1} ${num2}`;
+  return [question, answer];
 };
 
 export default () => runGame(description, getGameData);
--- src/game/prime.js
+++ src/game/prime.js
@@ -4,19 +4,19 @@ import runGame from '../main.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-    for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) {
-            return false;
-        }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
     }
-    return num > 1;
+  }
+  return num > 1;
 };
 
 const getGameData = () => {
-    const number = getRandomNumber(0, 50);
-    const question = `${number}`;
-    const answer = isPrime(number) ? 'yes' : 'no';
-    return [question, String(answer)];
+  const number = getRandomNumber(0, 50);
+  const question = `${number}`;
+  const answer = isPrime(number) ? 'yes' : 'no';
+  return [question, String(answer)];
 };
 
 export default () => runGame(description, getGameData);
